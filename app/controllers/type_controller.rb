887ff0fea5789d65d9f76b6e2d59ee81c28109e9
Sidra <alice@alum.mit.edu>
2015-12-30 21:56:33 -0500

    minor changes to wording

--- app/controllers/type_controller.rb
+++ app/controllers/type_controller.rb
@@ -3,8 +3,8 @@ class TypeController < ApplicationController
   def show
     if params[:id].blank?
       render :start
-    elsif params[:id] == "outline"
-      render :outline
+    elsif params[:id] == "overview"
+      render :overview
     else
       [Realm, Attitude, Quad].each do |klass|
         if klass.paths.include?(params[:id])
