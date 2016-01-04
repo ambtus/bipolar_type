d94aa3eacf18c36f4a45ee305e8b091b9752b7be
Sidra <alice@alum.mit.edu>
2016-01-04 16:34:54 -0500

    moved overview and renamed to theory

--- app/controllers/type_controller.rb
+++ app/controllers/type_controller.rb
@@ -3,14 +3,14 @@ class TypeController < ApplicationController
   def show
     if params[:id].blank?
       render :start
-    elsif params[:id] == "overview"
-      render :overview
+    elsif params[:id] == "theory"
+      render :theory
     else
       [Realm, Attitude, Quad].each do |klass|
         if klass.paths.include?(params[:id])
           @object = klass.send(params[:id])
           render :show and return
-        end 
+        end
       end
       raise "#{params[:id]} is not a realm, attitude, or quad"
     end
