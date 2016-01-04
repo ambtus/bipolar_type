f072f1e603b478552c0ed21ee7e0f38723e10206
Sidra <alice@alum.mit.edu>
2016-01-04 17:41:13 -0500

    added manic-depression advice and cleaned up the wording for the attitudes

--- app/models/realm.rb
+++ app/models/realm.rb
@@ -18,9 +18,10 @@ class Realm < Indexable
 
   def consume; choose Verb, %w{eat listen earn look}; end
   def consume_helper; choose Word, %w{NIL to NIL at}; end
+  def consume_with; consume_helper ? Phrase.new([consume, consume_helper]) : consume; end
 
   def produce; choose Verb, %w{move talk buy predict}; end
-  def produce_with; choose Verb, %w{do say own guess}; end
+  def produce_with; choose Verb, %w{do say own change}; end
 
   def unproductive; choose Adjective, %w{seated silent unprovisioned undecided}; end
 
@@ -29,21 +30,25 @@ class Realm < Indexable
   def empty; choose Adjective, %w{hungry lonely purse-pinched unsure}; end
   def worn_out; choose Adjective, %w{sore misunderstood maxed-out wrong}; end
 
+  def dying; choose Word, %w{starvation suicide destitution fatal\ mistakes}; end
+
   def strengths; choose Noun, %w{muscles metaphors credit rules}; end
   def kinetics; choose Noun, %w{glycogen emotions cash facts}; end
-  def potential; choose Noun, %w{fat attachments savings generalizations}; end
+  def potential; choose Noun, %w{fat attachments savings memories}; end
 
   def resources; choose Noun, %w{food sounds rewards pictures}; end
   def gain_resources; Phrase.new [consume, consume_helper, resources]; end
 
   def strengtheners; choose Noun, %w{protein words property patterns}; end
-  def energizers; choose Noun, %w{carbs intonation wages details}; end
+  def obvious; choose Adjective, %w{sweet loud overtime clear}; end
+  def energizers; choose Noun, %w{carbs tones wages facts}; end
   def buffers; choose Noun, %w{fat harmony automatic\ savings categories}; end
 
   def productions
-    ["go places and do things",
-     "win friends and influence people",
-     "obtain basic needs and pursue pleasure",
-     "solve problems and change the future"][index].to_phrase
+    [Phrase.new([Verb.new("go"), "places and", Verb.new("do"), "things"]),
+     Phrase.new([Verb.new("win"), "friends and", Verb.new("influence"), "people"]),
+     Phrase.new([Verb.new("obtain"), "basic needs and", Verb.new("pursue"), "pleasure"]),
+     Phrase.new([Verb.new("solve"), "problems and", Verb.new("change"), "the future"])
+    ][index]
   end
 end
