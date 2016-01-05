bbb241f83eb19fe47be8bfdc2b4d2a426fe621a7
Sidra <alice@alum.mit.edu>
2016-01-04 20:06:00 -0500

    more wording changes

--- app/models/realm.rb
+++ app/models/realm.rb
@@ -12,7 +12,7 @@ class Realm < Indexable
   end
   Attitude.all.each {|a| define_method(a.path) {self + a}}
 
-  def domain; choose Adjective, %w{physical social financial mental}; end
+  def domain; choose Adjective, %w{physical spiritual material mental}; end
   def domainly; domain.ly; end
   def name; Phrase.new [domain.titleize, parenthesize]; end
 
@@ -20,34 +20,34 @@ class Realm < Indexable
   def consume_helper; choose Word, %w{NIL to NIL at}; end
   def consume_with; consume_helper ? Phrase.new([consume, consume_helper]) : consume; end
 
-  def produce; choose Verb, %w{move talk buy predict}; end
-  def produce_with; choose Verb, %w{do say own change}; end
+  def produce; choose Verb, %w{walk talk shop decide}; end
+  def produce_with; choose Verb, %w{do say buy predict}; end
 
-  def unproductive; choose Adjective, %w{seated silent unprovisioned undecided}; end
+  def unproductive; choose Adjective, %w{lazy silent frugal undecided}; end
 
   def description; Phrase.new [consume, "and", produce]; end
 
-  def empty; choose Adjective, %w{hungry lonely purse-pinched unsure}; end
-  def worn_out; choose Adjective, %w{sore misunderstood maxed-out wrong}; end
+  def empty; choose Adjective, %w{hungry lonely overdrawn unsure}; end
+  def worn_out; choose Adjective, %w{sore misunderstood indebted wrong}; end
 
   def dying; choose Word, %w{starvation suicide destitution fatal\ mistakes}; end
 
   def strengths; choose Noun, %w{muscles metaphors credit rules}; end
   def kinetics; choose Noun, %w{glycogen emotions cash facts}; end
-  def potential; choose Noun, %w{fat attachments savings memories}; end
+  def potential; choose Noun, %w{fat personal\ opinions savings generalizations}; end
 
-  def resources; choose Noun, %w{food sounds rewards pictures}; end
+  def resources; choose Noun, %w{food stories rewards truths}; end
   def gain_resources; Phrase.new [consume, consume_helper, resources]; end
 
-  def strengtheners; choose Noun, %w{protein words property patterns}; end
+  def strengtheners; choose Noun, %w{protein words repayments results}; end
   def obvious; choose Adjective, %w{sweet loud overtime clear}; end
-  def energizers; choose Noun, %w{carbs tones wages facts}; end
-  def buffers; choose Noun, %w{fat harmony automatic\ savings categories}; end
+  def energizers; choose Noun, %w{carbs music wages facts}; end
+  def buffers; choose Noun, %w{fat shared\ opinions automatic\ savings categories}; end
 
   def productions
-    [Phrase.new([Verb.new("go"), "places and", Verb.new("do"), "things"]),
+    [Phrase.new([Verb.new("go"), "places and", Verb.new("lift"), "weights"]),
      Phrase.new([Verb.new("win"), "friends and", Verb.new("influence"), "people"]),
-     Phrase.new([Verb.new("obtain"), "basic needs and", Verb.new("pursue"), "pleasure"]),
+     Phrase.new([Verb.new("pursue"), "pleasure and", Verb.new("obtain"), "comfort and safety"]),
      Phrase.new([Verb.new("solve"), "problems and", Verb.new("change"), "the future"])
     ][index]
   end
