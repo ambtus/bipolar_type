4e01c24ecb8141636d41b446adeb30eea6b38a0f
Sidra <alice@alum.mit.edu>
2016-03-16 01:48:16 -0400

    almost there

--- app/models/realm.rb
+++ app/models/realm.rb
@@ -19,22 +19,22 @@ class Realm < Indexable
   def adverb; adjective.ly; end
   def name; adjective.titleize; end
 
-  def generic; choose Adjective, %w{ physical mental social financial}; end
+  def generic; choose Adjective, %w{ physical mental emotional financial}; end
 
   def consume; choose Verb, %w{eat look listen work}; end
   def consume_alt; choose Verb, %w{eat see hear earn}; end
-  def consume_with_verb; choose Verb, %w{eat look listen handle}; end
+  def consume_with_verb; choose Verb, %w{eat look listen do}; end
   def consume_with_helper; choose Word, %w{NIL at to NIL}; end
   def consume_with; Phrase.optional consume_with_verb, consume_with_helper; end
-  def part; choose Noun, %w{food color voice feature}; end
+  def part; choose Noun, %w{food color voice task}; end
   def parts; part.pluralize; end
-  def resource; choose Noun, %w{meal picture story tool}; end
+  def resource; choose Noun, %w{meal picture story job}; end
   def resources; resource.pluralize; end
 
   def stop; choose Verb, %w{leave close turn put}; end
   def the; choose Word, %w{the your off\ the down\ the}; end
   def stimulation; choose Noun, %w{kitchen eyes sound tools}; end
-  def shut_down; Phrase.new [stop, the, stimulation] ; end
+  def turn_off; Phrase.new [stop, the, stimulation] ; end
 
   def increase; choose Verb, %w{add turn turn take}; end
   def your; choose Word, %w{some on\ some up\ the some}; end
@@ -43,13 +43,14 @@ class Realm < Indexable
 
 
 
-  def energy; choose Noun, %w{food colors intonation rewards}; end
+  def energy; choose Noun, %w{food details intonation rewards}; end
 
   def potential; choose Noun, %w{fat memories friends savings}; end
   def fat; choose Adjective, %w{fat trivial connected rich}; end
   def thin; choose Adjective, %w{skinny theoretical isolated poor}; end
 
   def produce; choose Verb, %w{walk guess talk shop}; end
+  def produce_alt; choose Verb, %w{do predict say buy}; end
   def produce_with; choose Verb, %w{do solve influence acquire}; end
   def thing; choose Noun, %w{thing problem person possession}; end
   def things; thing.pluralize; end
@@ -62,10 +63,10 @@ class Realm < Indexable
   def appears; appear.s; end
   def appearance; choose Noun, %w{smell sight sound touch}; end
   def appearances; appearance.pluralize; end
-  def bad; choose Adjective, %w{bitter ugly whiny dangerous }; end
+  def bad; choose Adjective, %w{bitter ugly whiny difficult }; end
   def good; choose Adjective, %w{sweet symmetric harmonious easy}; end
-  def obvious; choose Adjective, %w{fragrant bright loud rough}; end
-  def subtle; choose Adjective, %w{bland pastel soft smooth}; end
+  def obvious; choose Adjective, %w{fragrant bright loud painful}; end
+  def subtle; choose Adjective, %w{bland pastel soft repetitive }; end
 
   def sick; choose Verb, %w{nauseous afraid ashamed angry};end
   def be_sick_verb; choose Verb, %w{throw panic cry fight};end
