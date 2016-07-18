796ed6cf83c2de468e58e16c00639e50ba7135df
Sidra <alice@alum.mit.edu>
2016-07-18 09:57:38 -0400

    removed answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'start#index'
   resources :start, only: [:index, :show]
-  resources :answers, only: [:show]
   resources :realms, only: [:show]
   resources :attitudes, only: [:show]
   resources :subtypes, only: [:index, :show]
