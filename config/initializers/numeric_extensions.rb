5f515b66da31aa7dd8ae583edd25a09f053d0c93
Sidra <alice@alum.mit.edu>
2017-10-12 09:33:43 -0400

    bundle update

--- config/initializers/numeric_extensions.rb
+++
@@ -1,9 +0,0 @@
-# Restart required even in development mode when you modify this file.
-
-class Fixnum
-  def word
-    %w{zero one two three four five six seven eight nine
-       ten eleven twelve thirteen fourteen fifteen
-       sixteen seventeen eighteen nineteen twenty}[self] || self.to_s
-  end
-end
