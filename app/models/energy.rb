43e4af738e1618f001ed6ed41d5c22a883b3a0c2
Sidra <alice@alum.mit.edu>
2018-02-10 10:35:09 -0500

    with links and some energy info

--- app/models/energy.rb
+++ app/models/energy.rb
@@ -1,9 +1,16 @@
 class Energy < Concept
 
-  ENERGIES = %w{physical mental financial social}
+  ADJECTIVES = %w{physical mental financial social}
+  GETS = %w{eat watch earn listen}
+  USES = %w{walk choose buy talk}
+  PRODUCTIONS = %w{exercise think spend communicate}
+  MEASURES = %w{calories information money emotions}
+  TRIGGERS = ["sweet carbohydrates", "colorful details", "cash payments", "musical intonations"]
+  BUFFERS = ["fats and proteins", "black and white shapes and outlines",
+             "credits or deposits", "written words"]
 
   ########
-  ACRONYMS = ENERGIES.map(&:first)
+  ACRONYMS = ADJECTIVES.map(&:first)
   ALL = ACRONYMS.collect {|letter| self.new letter}
   ACRONYMS.each do |letter|
     define_singleton_method(letter) {ALL[ACRONYMS.index(letter)]}
@@ -13,8 +20,15 @@ class Energy < Concept
   def subtypes; Subtype.all.select{|s| s.energy == self}; end
   def +(state); subtypes.find{|s| s.state == state}; end
 
-  def energy; ENERGIES[index]; end
-  def name; energy.capitalize; end
+  def adjective; ADJECTIVES[index]; end
+  def name; adjective.capitalize; end
   def symbol; path.upcase; end
+
+  def get; GETS[index]; end
+  def use; USES[index]; end
+  def produce; PRODUCTIONS[index]; end
+  def energy; MEASURES[index]; end
+  def triggers; TRIGGERS[index]; end
+  def buffers; BUFFERS[index]; end
 
 end
