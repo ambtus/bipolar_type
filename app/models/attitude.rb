135e595e7f812c3849e1d3c69dacc5f1daa88400
Sidra <alice@alum.mit.edu>
2018-07-17 10:23:41 -0400

    stash

--- app/models/attitude.rb
+++ app/models/attitude.rb
@@ -1,7 +1,7 @@
 class Attitude < Concept
 
   ########
-  ACRONYMS = %w{uf of uw ow}
+  ACRONYMS = %w{ej ij ip ep}
   ALL = ACRONYMS.collect {|letter| self.new letter}
   ACRONYMS.each do |letter|
     define_singleton_method(letter) {ALL[ACRONYMS.index(letter)]}
@@ -14,17 +14,7 @@ class Attitude < Concept
   def subtypes; Subtype.all.select{|s| s.attitude == self}; end
   def +(realm); subtypes.find{|s| s.realm == realm}; end
 
-  def u_or_o; path.first == "u" ? "under" : "over"; end
-  def c_or_p; path.second == "f" ? "fuel" : "work"; end
-  def description; [u_or_o, c_or_p].join(" "); end
+  def adjective; %w{productive responsible happy active}[index]; end
+  def name; adjective.titleize; end
 
-  def noun; %w{productive responsible content active}[index]; end
-  def episode; index < 2 ? "mania" : "depression"; end
-  def description; [noun, episode].join(" "); end
-  def name; noun.titleize; end
-
-  def result; %w{thin fat weak strong}[index]; end
-  def goal; %w{fat thin strong weak}[index]; end
-  def good_behavior; %w{recharging fasting laboring resting}[index]; end
-  def problem; %w{manic depressed depressed manic}[index]; end
 end
