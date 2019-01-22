ce26f9d34ac1133bbe83aad51f62c1fb5fde6e11
Sidra <alice@alum.mit.edu>
2019-01-22 14:17:36 -0500

    stash

--- app/models/attitude.rb
+++ app/models/attitude.rb
@@ -13,11 +13,7 @@ class Attitude < Concept
   def subtypes; Subtype.all.select{|s| s.attitude == self}; end
   def +(realm); subtypes.find{|s| s.realm == realm}; end
 
-  def adjective; %w{depressed conservative liberal manic}[index]; end
-
-  def outside?; [0,3].include?(index); end
-
-  def health; outside? ? "dangerous" : "unhealthy" ;end
-  def episode; index > 1 ? "mania" : "depression"; end
+  def adjective; %w{fat weak thin strong}[index]; end
+  def state; index < 2 ? "depressed" : "manic"; end
 
 end
--- app/models/realm.rb
+++ app/models/realm.rb
@@ -14,31 +14,40 @@ class Realm < Concept
 
   def adjective; %w{physical mental affective financial}[index]; end
 
-  def energy; %w{glucose data affect money}[index]; end
-  def provide; %w{feed show tell hand}[index]; end
+  def resource; %w{food answer story tool}[index]; end
+  def resources; resource.pluralize; end
+  def baduns; %w{poisons lies bigotry junk}[index]; end
+  def energy; %w{calories data affect currency}[index]; end
+  def give; %w{feed show give hand}[index]; end
   def notice; %w{smell see hear touch}[index]; end
-  def collect_w; %w{eat learn listen\ to fight\ for}[index]; end
-  def collect; collect_w.split.first; end
-  def process; %w{digest remember agree\ with win}[index]; end
-  def resources; %w{foods facts opinions rewards}[index]; end
+  def use; %w{eat look\ at listen\ to pick\ up}[index]; end
+  def use_wo; use.split.first; end
+  def refuel; [use, resources].join(" "); end
+  def process; %w{digest believe agree\ with use}[index]; end
+  def process_wo; process.split.first; end
+  def processable; process_wo.gsub(/e?$/, '') + "able"; end
   def appear; %w{taste look sound feel}[index]; end
-  def gather; [collect_w, resources].join(" "); end
-  def harvest; "#{notice}, #{collect_w}, and #{process} #{resources} that #{appear} right"; end
-  def energizers; %w{carbs specifics compliments battles}[index]; end
-  def strengtheners; %w{proteins generalities criticisms wars}[index]; end
-  def reserves; %w{fat knowledge values savings}[index]; end
+  def neuro; %w{serotonin norepinephrine oxytocin dopamine}[index]; end
+  def energizer; %w{carb fact compliment weapon}[index]; end
+  def strengthener; %w{protein supposition criticism defense}[index]; end
+  def buffers; %w{fat symmetry laughter handles}[index]; end
   def empty; %w{hungry curious lonely overdrawn}[index]; end
+  def parts; [energizer.s, strengthener.s, buffers].to_sentence; end
+  def reserves; %w{fat knowledge self-esteem savings}[index]; end
+  def sick; %w{nauseated afraid unhappy angry}[index]; end
+  def organ; %w{mouth eyes ears hands}[index]; end
 
-  def practice; %w{walk think talk pay}[index]; end
-  def accomplish; %w{go evaluate tell own}[index]; end
+  def act; %w{walk think talk pay}[index]; end
+  def accomplish; %w{go organize befriend buy}[index]; end
   def goal; %w{place idea person thing}[index]; end
   def goals; goal.pluralize; end
   def anything; %w{anywhere anything anyone anything}[index]; end
   def something; %w{somewhere something someone something}[index]; end
   def them_there; index > 0 ? "them" : "there"; end
-  def easy; %w{flat simple allied cheap}[index]; end
-  def hard; %w{high complex unrelated expensive}[index]; end
-  def labor; [accomplish, goals].join(" "); end
-  def strength; %w{muscle intelligence eloquence credit}[index]; end
+  def easy; %w{level simple allied cheap}[index]; end
+  def hard; %w{high complex unfamiliar expensive}[index]; end
+  def labor; [accomplish, hard, goals].join(" "); end
+  def relax; [accomplish, easy, goals].join(" "); end
+  def strength; %w{muscle intelligence vocabulary credit}[index]; end
 
 end
