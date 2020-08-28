0aaa37306a55c3eb73e2f1d59b967b70d1a09ecc
Sidra <alice@alum.mit.edu>
2020-08-28 07:16:14 -0400

    stash

--- app/models/attitude.rb
+++ app/models/attitude.rb
@@ -17,22 +17,11 @@ class Attitude < Concept
   def subtypes; Subtype.all.select{|s| s.attitude == self}; end
   def +(realm); subtypes.find{|s| s.realm == realm}; end
 
-  def position; %w{  south west north east}[index]; end
   def opposite; ALL[(index + 2).modulo(4)]; end
   def previous; ALL[(index + 3).modulo(4)]; end
   def subsequent; ALL[(index + 1).modulo(4)]; end
 
-  def season; %w{ spring summer fall winter}[index]; end
-  def time; %w{ morning day evening night}[index]; end
-  def time_period; [season, time].to_phrase ; end
 
-  def beginning; %w{ dawn mid-morning mid-afternoon dusk}[index]; end
-  def ending; subsequent.beginning; end
-  def mid; %w{ sunrise noon sunset midnight}[index]; end
-
-  def behavior; %w{ work rest\ &\ refresh play sleep\ &\ dream}[index]; end
-  def energy_flow; %w{ use\ stored\ energy\ to\ achieve\ goals get\ fresh\ energy use\ fresh\ energy\ to\ pursue\ happiness conserve\ energy}[index]; end
-
-  def name; time.titleize; end
+  def name; symbol; end
 
 end
--- app/models/realm.rb
+++ app/models/realm.rb
@@ -15,36 +15,10 @@ class Realm < Concept
   def subtypes; Subtype.all.select{|s| s.realm == self}; end
   def +(attitude); subtypes.find{|s| s.attitude == attitude}; end
 
-  def adjective; %w{affective physical financial mental}[index]; end
+  def adjective; %w{social physical material mental}[index]; end
   def name; adjective.capitalize; end
 
-  def verb; %w{influence go\ to improve organize}[index]; end
-  def noun; %w{person place thing idea}[index]; end
-  def nouns; noun.pluralize; end
-  def mine; %w{family bedroom investments theories}[index]; end
-  def work; %w{write garden invest think}[index]; end
-  def play; %w{talk dance splurge guess}[index]; end
-  def refresh; %w{listen eat compete watch}[index]; end
-  def dream(whose="my"); [verb, whose, mine].to_phrase; end
+  def get; %w{ listen eat earn learn}[index]; end
+  def use; %w{ talk move spend think}[index]; end
 
-  def work_others; %w{write hunt/gather/cook buy/build/upgrade/repair prove}[index]; end
-
-  def thing; %w{body where thing thing}[index]; end # some#{thing}, no#{thing}, etc.
-  def it; index == 0 ? "them" : "it"; end
-
-  def use; %w{express burn spend synthesize}[index]; end
-  def energy; %w{love calories money facts}[index]; end
-  def reserves; %w{values fat savings knowledge}[index]; end
-  def on; %w{to in on into}[index]; end
-
-  def take; %w{hear taste collect watch}[index]; end
-  def source; %w{response food reward result}[index]; end
-  def sources; source.pluralize; end
-
-  def refresh_long; [take, "the", sources].to_phrase; end
-
-  def overwhelmed; %w{ashamed disgusted angry afraid}[index]; end
-
-  def sleep; "preserve #{energy} as #{reserves}"; end
-  def dream_long(who="I"); "consider the #{nouns} #{who} want to #{verb} before #{who} start #{work.ing}"; end
 end
