791662d4d54255ad40e647565884deca8d361bd3
Sidra <alice@alum.mit.edu>
2022-06-03 07:30:43 -0400

    answer snapshot

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,35 +4,29 @@ class Answer
   def self.first; Answer.new(first_path); end
 
   def initialize(string)
-    @question,subtypes = string.split(":")
-    @subtype_paths = (subtypes || "").split("-")
+    @question,subtype_string = string.split(":")
+    @subtype_string = subtype_string || ""
   end
-  attr_reader :question, :subtype_paths
+  attr_reader :question
 
   def number; @question.last.to_i ; end
   def index; number - 1; end
   def finished?; number > 4; end
 
-
-  def chosen; subtype_paths.collect{|path| Subtype.send(path)}; end
-  def constrained; chosen.map(&:siblings).flatten.uniq; end
-
-  def css(subtype)
-    if chosen.include? subtype
-      "chosen"
-    elsif constrained.include? subtype
-      "warning"
-    else
-      "free"
-    end
+  def css(choice)
+    return "chosen" if subtypes.include?(choice)
+    return "constrained" if positions.include?(choice.first)
+    return "constrained" if realms.include?(choice.second)
+    return "free"
   end
 
-  def all(subtype); chosen << subtype; end
-  def paths(subtype); all(subtype).map(&:path).join('-'); end
-  def next(subtype); "#{question.next}:#{paths(subtype)}"; end
+  def next(choice); question.next + ":" + @subtype_string + choice; end
 
-  def subtypes; (Subtype.all - constrained).sort; end
-  def type_path; subtypes.map(&:realm).map(&:path).join; end
+  def type_path; subtypes.sort.map(&:second).join; end
 
+  private
+  def subtypes; @subtype_string.scan(/../); end
+  def realms; subtypes.map(&:second); end
+  def positions; subtypes.map(&:first); end
 
 end
