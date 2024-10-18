7f4680f6fd2053d30538ed5a5a71d036467b33d0
Sidra <alice@alum.mit.edu>
2024-10-18 08:55:03 -0400

    cookie bug

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -10,28 +10,22 @@ class CookiesController < ApplicationController
       cookies[:setting] = 'words'
     when 'Both'
       cookies.delete('setting')
-    when 'Save'
+    when 'Save', nil
       words = Rails.application.config_for(:words)
       words.keys.each do |k|
         if params.has_key? k
-          if params[k].blank? || (params[k] == words[k])
+          if params[k].blank? || (params[k] == words[k].first)
             cookies.delete(k)
           else
             cookies[k] = params[k]
           end
         end
       end
-    when nil
-      @words = Rails.application.config_for(:words)
-      @words.keys.each do |k|
-        if params.has_key? k
-          cookies[k] = params[k]
-        else
-          cookies.delete(k)
-        end
+      if params[:commit].blank?
+        @words = words
+        @url = cookies_path(cookies.to_hash)
+        render 'cookies' and return
       end
-      @url = cookies_path(cookies.to_hash)
-      render 'cookies' and return
     end
 
     redirect_back fallback_location: root_path
