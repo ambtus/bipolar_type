77e4e2074d38760ac21fa16c05a2ac1f90260057
Sidra <alice@alum.mit.edu>
2024-10-30 09:36:21 -0400

    oops

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
   # corresponding to the log level numbers from 0 up to 5 respectively.
   config.log_level = :info
   config.hosts << '.bipolartype.com'
+  config.hosts << 'bipolar-type.onrender.com'
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
