a22ec2bc1e2966100c199c9879fd00cc6c57a13c
Sidra <alice@alum.mit.edu>
2025-06-11 14:11:40 -0400

    swapped ideas and things
    usually, kids have to start learning before they have to start earning

--- app/models/realm.rb
+++ app/models/realm.rb
@@ -1,6 +1,6 @@
 class Realm < Concept
 
-  SYMBOL = %i{F S T N }
+  SYMBOL = %i{F S N T}
 
   ALL = SYMBOL.collect {|symbol| self.new symbol}
   SYMBOL.each do |sym|
@@ -10,22 +10,21 @@ class Realm < Concept
   def subtypes; Subtype.all.select{|x| x.realm == self}; end
   def +(attitude); subtypes.find{|x| x.attitude == attitude}; end
 
-  def focus; %w{person place thing idea}[index]; end
+  def focus; %w{person place idea thing}[index]; end
   def foci; focus.pluralize; end
   def name; foci.capitalize; end
 
-  def adjective; %w{social physical financial mental}[index]; end
-  def adverb; adjective.ly; end
+  def adjective; %w{social physical mental financial}[index]; end
   def letter; adjective.first.capitalize; end
 
-  def get; %w{listen\ to eat earn learn}[index]; end
-  def energy; %w{music carbs wages facts}[index]; end
-  def strength; %w{sermons protein a\ salary rules}[index]; end
+  def get; %w{listen\ to eat learn earn}[index]; end
+  def energy; %w{music carbs facts wages}[index]; end
+  def strength; %w{sermons protein rules a\ salary}[index]; end
   def get_energy; [get, energy].to_phrase; end
   def get_strength; [get, strength].to_phrase; end
 
-  def use; %w{communicate move buy think}[index]; end
-  def use_energy; %w{sing gather pay\ cash choose}[index]; end
-  def use_strength; %w{preach hunt use\ credit decide}[index]; end
+  def use; %w{communicate move think buy}[index]; end
+  def use_energy; %w{sing gather choose pay\ cash}[index]; end
+  def use_strength; %w{preach hunt decide use\ credit}[index]; end
 
 end
